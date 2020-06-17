0f372296d8abbe6cc28ea1a60bab89f49fd4e9d2
root-ansh <anshsachdevaprofessional@gmail.com>
2020-06-17 09:04:28 +0530

    prev comment

---
+++ code/app/src/main/java/in/curioustools/menu_maker/modal/MenuEntryType.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2020.
+ * created on 17/6/20 9:01 AM
+ * by  Ansh Sachdeva (www.github.com/root-ansh)
+ *
+ */
+
+package in.curioustools.menu_maker.modal;
+
+public enum MenuEntryType {CATEGORY,ITEM}
